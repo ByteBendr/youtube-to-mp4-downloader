0c3fed14db44202a211b59a8eab889169e3409ac
ByteBendr <138248676+ByteBendr@users.noreply.github.com>
2023-10-27 14:12:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function downloadVideo() {
     const videoId = document.getElementById('searchInput').value;
-    window.location.href = `https://youtube-to-mp4-downloader.vercel.app/download?url=${videoId}`;
+    window.location.href = `https://youtubetomp4.onrender.com/download?url=${videoId}`;
 }
